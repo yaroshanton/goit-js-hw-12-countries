c9799f6630a8f7a97c8ff5cecd27c583d2b5cf00
Anton Yarosh <on.yaroshanton@gmail.com>
2020-11-01 14:09:31 +0200

    nothing important

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import fetchArticles from "./js/fetchCountries";
 import createManyArticles from "./templates/createManyArticles.hbs";
 import createOneArticle from "./templates/createOneArticle.hbs";
 import createArticlesMurkup from "./js/createArticlesMurkup";
-import { error, defaultModules,} from "@pnotify/core/dist/PNotify.js";
+import { error, defaultModules, } from "@pnotify/core/dist/PNotify.js";
 import '@pnotify/core/dist/BrightTheme.css';
 
 import lodash from "lodash";
@@ -14,7 +14,7 @@ function countrySelection(countries) {
     return createArticlesMurkup(countries, createOneArticle);
   } else if (countries.length > 10) {
     error("Too many matches found. Please enter a more specific query!");
-  } else if (countries.length > 1 && countries.length < 10)  {
+  } else if (countries.length > 1 && countries.length < 10) {
     return createArticlesMurkup(countries, createManyArticles);
   }
 }
